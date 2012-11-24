110ec8a220c960fff9ae42eab01c373e1ade427c
VertigoRay <github.com@80.vertigion.com>
2012-11-24 13:09:46 -0600

    django-registration
    Added registration per the QuickStart:
    http://docs.b-list.org/django-registration/0.8/quickstart.html
    It's not functional yet. The pages don't actually do anything yet.

--- settings.py
+++ settings.py
@@ -50,7 +50,7 @@ MEDIA_ROOT = '/home/lurkin4life/com.worleybox.csce4350/public/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://csce4350.worleybox.com/media'
+MEDIA_URL = 'http://csce4350.worleybox.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -116,10 +116,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'registration',
     # csce4350 apps
     'profile',
     'shop',
@@ -157,6 +156,10 @@ LOGGING = {
 # For UserProfile
 #   Ref:  http://stackoverflow.com/a/965883/615422
 AUTH_PROFILE_MODULE = 'profile.models.UserProfile'
+
+# django-registration
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
 
 import os
 if os.uname()[1] == 'dev-v3pu':
--- urls.py
+++ urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^account/', include('registration.backends.default.urls')),
+
     url(r'^profile/', include('profile.urls', namespace='profile')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 )
