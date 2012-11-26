c3aa00de1a7714354670dedc713896bc0bad2c68
VertigoRay <github.com@80.vertigion.com>
2012-11-26 16:18:12 -0600

    /profile/ adjustments

--- profile/views.py
+++ profile/views.py
@@ -4,9 +4,15 @@ from django.template import Context, loader
 from profile.models import *
 
 def index(request):
-    user_list = get_list_or_404(User.objects.order_by('username'))
-    return render(request, 'profile/index.html', {'user_list': user_list})
+    variables = RequestContext(request, {
+        'title':    'Profiles',
+        'user_list':  get_list_or_404(User.objects.order_by('username')),
+    })
+    return render(request, 'profile/index.html', variables)
 
 def detail(request, username):
-    user = get_object_or_404(User, username=username)
-    return render(request, 'profile/detail.html', {'user': user})
+     variables = RequestContext(request, {
+        'title': username,
+        'user':	get_object_or_404(User, username=username),
+    })
+    return render(request, 'profile/detail.html', variables)
