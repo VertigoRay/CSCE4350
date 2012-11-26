f88057876d4aec83735a600fa03fdc4672cd48fb
Lurkin4Life <WorleyC@gmail.com>
2012-11-25 23:27:49 -0600

    Update profile/sql/rating.sql

--- profile/sql/rating.sql
+++ profile/sql/rating.sql
@@ -1,1 +1,17 @@
 -- rating.sql
+-- USER RATING TABLE -- --verified
+INSERT INTO profile_rating (buyer_id, seller_id, rating, comment) VALUES
+    (1, 4, 8, 'seller was good responded quickly, fair asking price'),
+    (2, 6, 9, 'responded quickly, fair asking price'),
+    (2, 6, 10, 'good asking price, quick response'),
+    (3, 2, 8, 'seller was good, good asking price'),
+    (4, 5, 1, 'Awful seller item broke in 10 days'),
+    (5, 2, 10, 'seller was good responded quickly, fair asking price'),
+    (6, 7, 8, 'Good Seller, very good description'),
+    (7, 12, 9, 'responded quickly, fair asking price'),
+    (8, 11, 8, 'seller was good and nice'),
+    (9, 5, 7, 'seller was good responded quickly'),
+    (10, 3, 10, 'awesome seller!'),
+    (11, 7, 3, 'seller was bad, not as described'),
+    (12, 9, 8, 'fair asking price')
+;
