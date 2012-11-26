69ecf4f8018386625ec53100022416f48414938c
VertigoRay <github.com@80.vertigion.com>
2012-11-26 15:24:38 -0600

    SETTINGS_PATH not defined

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SETTINGS_PATH, 'public', 'media')
+MEDIA_ROOT = '%s/public/media/' % os.path.dirname(os.path.realpath(__file__))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,7 +57,7 @@ MEDIA_URL = 'http://csce4350.worleybox.com/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SETTINGS_PATH, 'static')
+STATIC_ROOT = '%s/static/' % os.path.dirname(os.path.realpath(__file__))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SETTINGS_PATH, 'templates'),
+    '%s/templates' % os.path.dirname(os.path.realpath(__file__)),
 )
 
 INSTALLED_APPS = (
