d4f77e7bb5ce3c906b5aba45b858d915900a270a
VertigoRay <github.com@80.vertigion.com>
2012-11-26 00:02:40 -0600

    fixed DH?
    & tweaked decimals

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,9 @@
 import sys, os
 
+INTERP = os.path.join(os.environ['HOME'], '.virtualenvs', 'project_name', 'bin', 'python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
 sys.path.insert(0, '/home/lurkin4life/.virtualenv/csce4350/lib/python2.5/site-packages')
 sys.path.append(os.getcwd())
 
