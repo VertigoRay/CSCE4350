c2e316f90474a466fbd7cbf499c76ad02912ce3a
VertigoRay <github.com@80.vertigion.com>
2012-11-25 20:29:02 -0600

    passenger_wsgi ref .virtualenv
    Should fix internal server error?

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,9 @@
 import sys, os
+
+sys.path.insert(0, '/home/lurkin4life/.virtualenv/csce4350/lib/python2.5/site-packages')
 sys.path.append(os.getcwd())
+
 os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
+
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
