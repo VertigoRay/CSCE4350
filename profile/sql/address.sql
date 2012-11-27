53904925516d8329a4f70b1a099d11c9e932dd81
Chris Worley <WorleyC@gmail.com>
2012-11-27 00:23:22 -0600

    deleted billing address field. fixing user address profile

--- profile/sql/address.sql
+++ profile/sql/address.sql
@@ -2,27 +2,15 @@
 -- ADDRESS TABLE -- --verified
 INSERT INTO profile_address (user_id, addtype, addr1, addr2, city, state, zipcode) VALUES
     (2, 's','234 Main St',null,'Fort Worth','TX','76179'),
-	(2, 'b','234 Main St',null,'Fort Worth','TX','76179'),
 	(3, 's','345 Main St',null,'Denton','TX','76201'),
-	(3, 'b','345 Main St',null,'Denton','TX','76201'),
 	(4, 's','456 Main St',null,'Dallas','TX','76022'),
-	(4, 'b','456 Main St',null,'Dallas','TX','76022'),
 	(5, 's','567 Main St',null,'Oklahoma City','OK','74044'),
-	(5, 'b','567 Main St',null,'Oklahoma City','OK','74044'),
 	(6, 's','678 Main St',null,'Chicago','CO','45004'),
-	(6, 'b','678 Main St',null,'Chicago','CO','45004'),
 	(7, 's','789 Main St',null,'Portland','OR','12345'),
-	(7, 'b','789 Main St',null,'Portland','OR','12345'),
 	(8, 's','890 Main St',null,'Austin','TX','76005'),
-	(8, 'b','890 Main St',null,'Austin','TX','76005'),
 	(9, 's','1234 Main St',null,'Chicago','CO','45004'),
-	(9, 'b','1234 Main St',null,'Chicago','CO','45004'),
 	(10, 's','2345 Main St',null,'Orlando','FL','23456'),
-	(10, 'b','2345 Main St',null,'Orlando','FL','23456'),
 	(11, 's','3456 Main St',null,'Fresno','CA','85003'),
-	(11, 'b','3456 Main St',null,'Fresno','CA','85003'),
 	(12, 's','4567 Main St',null,'Chicago','CO','45004'),
-	(12, 'b','4567 Main St',null,'Chicago','CO','45004'),
-	(13, 's','123 Main St',null,'Denton','TX','76210'),
-	(13, 'b','123 Main St',null,'Denton','TX','76210')
+	(13, 's','123 Main St',null,'Denton','TX','76210')
 ;
