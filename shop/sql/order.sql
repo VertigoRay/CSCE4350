b212d2e54d994be5117f48c7c7797a495f8e7f0f
Chris Worley <WorleyC@gmail.com>
2012-11-27 02:24:57 -0600

    fix bmarley buying his own shee

--- shop/sql/order.sql
+++ shop/sql/order.sql
@@ -4,7 +4,7 @@
 INSERT INTO `shop_order` (user_id, product_id, billing_id, status, statusinfo,mod_date, pub_date) VALUES
   (2,12,2,'sr','USPS: 9405511201080473337287',null,null),
 	(2,15,2,'sr','UPS: 1Z2807700363847562',null,null),
-	(2,18,2,'or',null,null,null),
+	(3,18,3,'or',null,null,null),
 	(3,21,3,'sr','Fedex: 9002901001300270295452',null,null),
 	(3,24,3,'or',null,null,null),
 	(4,27,4,'sr','USPS: 9405511201080432879465',null,null),
