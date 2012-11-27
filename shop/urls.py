ad658ec10e8164ac9509895b764a0c83c723cd08
VertigoRay <github.com@80.vertigion.com>
2012-11-27 03:17:15 -0600

    added product add link and foo  ;)

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from shop import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<product_id>\d+)/$', views.product),
-    url(r'^(?P<category>\w+)/$', views.category),
-    url(r'^productadd/$', views.product_add, name='index'),
+    url(r'^add/$', views.product_add, name='add'),
+    url(r'^(?P<product_id>\d+)/$', views.product, name='product'),
+    url(r'^(?P<category>\w+)/$', views.category, name='category'),
 )
