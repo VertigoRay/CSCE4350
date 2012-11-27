19f9f3b0bbd16893832512a6e4fc8b8bb5be6e9d
Lurkin4Life <WorleyC@gmail.com>
2012-11-26 18:57:09 -0600

    Update profile/views.py

--- profile/views.py
+++ profile/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserProfile
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.template import Context, loader, RequestContext
 from profile.models import *
@@ -14,6 +14,6 @@ def detail(request, username):
     variables = RequestContext(request, {
         'title': username,
         'user_profile':	get_object_or_404(User, username=username),
-        'user_phone': get_object_or_404(UserProfile, "blah"),
+        'user_phone': get_object_or_404(UserProfile, phone=phone),
     })
     return render(request, 'profile/detail.html', variables)
