23fef385aa525533214f04720be5f76a459e1803
Chris Worley <WorleyC@gmail.com>
2012-11-27 23:15:03 -0600

    testing condition

--- shop/views.py
+++ shop/views.py
@@ -36,6 +36,7 @@ def product(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     title = product.title
     condition = product.get_condition_display()
+    print condition
     variables = RequestContext(request, {
         'title': title,
         'product': product,
