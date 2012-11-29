b302be7f364150c80936fc0ca422a7e248faaec8
Jimi Mills <Jimi@Jimis-MacBook-Pro.local>
2012-11-28 23:31:15 -0600

    fix stuff
    ...

--- views.py
+++ views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.template import RequestContext
+from shop.models import *
 
+# def index(request):
+#     variables = RequestContext(request, {
+#         'title':    'Home',
+#     })
+#     return render(request, 'index.html', variables)
 def index(request):
     variables = RequestContext(request, {
-        'title':    'Home',
+        'category': Category.objects.filter().order_by('pid__name', 'name'),
     })
-    return render(request, 'index.html', variables)
+    return render(request, 'shop/index.html', variables)
 
 def account_index(request):
     return render(request, 'registration/index.html')
